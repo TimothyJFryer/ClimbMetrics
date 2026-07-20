35d1c0ff10c821391445e29d7b278a1aadec6a80
timot <timothyjfryer@gmail.com>
2026-07-20 01:57:27 +0100

    initial frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+
+            <main>
+                <h1>Welcome to CruxVision</h1>
+            </main>
+        </div>
+    );
+}
+
+export default App;
