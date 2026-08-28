b017047683372f6b4f4b1dd8777a572fe067a15b
timot <timothyjfryer@gmail.com>
2026-08-28 02:24:42 +0100

    test: failed test actions fix

--- backend/src/test/java/com/climbmetrics/backend/AuthServiceTest.java
+++ backend/src/test/java/com/climbmetrics/backend/AuthServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -30,6 +32,13 @@ class RegistrationIntegrationTest {
                     .withDatabaseName("climbmetrics_test")
                     .withUsername("test")
                     .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Autowired
     private MockMvc mockMvc;
