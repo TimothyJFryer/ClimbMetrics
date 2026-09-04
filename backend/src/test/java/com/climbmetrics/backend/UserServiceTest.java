10dafb92df55e77de8ab8a0126ecd4c6d6a993f5
timot <timothyjfryer@gmail.com>
2026-09-04 13:56:46 +0100

    tests:Auth-4 added

--- backend/src/test/java/com/climbmetrics/backend/UserServiceTest.java
+++ backend/src/test/java/com/climbmetrics/backend/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.climbmetrics.backend;
 
+import com.climbmetrics.backend.dto.EditRequest;
 import com.climbmetrics.backend.entity.User;
 import com.climbmetrics.backend.repository.UserRepository;
 import com.climbmetrics.backend.service.JwtService;
@@ -8,20 +9,24 @@ import jakarta.servlet.http.Cookie;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 
@@ -41,6 +46,13 @@ class ProfileTests {
                     .withDatabaseName("climbmetrics_test")
                     .withUsername("test")
                     .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -109,6 +121,43 @@ class ProfileTests {
                 )
 
                 .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void userCanEditOwnProfile() throws Exception {
+        User user = new User();
+
+        user.setEmail("test@example.com");
+        user.setUsername("testuser");
+        user.setPassword("password");
+        user.setDescription("Test climber");
+
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user.getEmail());
+
+
+
+
+
+
+        mockMvc.perform(
+                        put("/api/profile/editProfile")
+                                .cookie(
+                                        new Cookie("accessToken", token)
+                                )
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                            {
+                                "email": "newEmail@example.com",
+                                "username": "newUsername",
+                                "description": "new description"
+                            }
+                            """)
+                )
+                .andExpect(status().is2xxSuccessful());
+
     }
 
 
